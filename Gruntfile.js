cee710fa778895439419f2fc9bd3bf3a91cb3ca8
Patrick McKinney <pnmcartography@gmail.com>
2020-07-24 09:04:23 -0400

    Remove whitespace in Grunt file
    Run plugins bundled update

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt){
 						block: true,
 						line: true
 					},
-				},	
+				},
 				src: ['plugins/js/esri/esri-leaflet/esri-leaflet.js', 'plugins/js/esri/esri-leaflet-renderers/esri-leaflet-renderers.js', 'plugins/js/esri/esri-leaflet-geocoder/esri-leaflet-geocoder.js', 'plugins/js/esri/esri-leaflet-legend/esri-leaflet-legend.js'],
 				dest: 'combined/js/esri/esri-leaflet-plugins.js'
 			},
@@ -26,7 +26,7 @@ module.exports = function(grunt){
 					stripBanners: {
 						block: true,
 						line: true
-					},	
+					},
 				},
 				src: ['plugins/js/leaflet/leaflet-fullscreen/Leaflet.fullscreen.js', 'plugins/js/leaflet/leaflet-locate-control/L.Control.Locate.min.js', 'plugins/js/leaflet/leaflet-zoomhome/leaflet.zoomhome.js'],
 				dest: 'combined/js/leaflet/leaflet-plugins.js'
@@ -35,11 +35,11 @@ module.exports = function(grunt){
 				src: ['plugins/js/proj4JS/proj4js/proj4.js', 'plugins/js/proj4JS/Proj4Leaflet/proj4leaflet/js'],
 				dest: 'combined/js/projections/projections.js'
 			}
-		},		
+		},
 		cssmin: {
           options: {
               sourceMap: true
-          },    
+          },
 		  target: {
 			files: [{
 			  expand: true,
@@ -53,7 +53,7 @@ module.exports = function(grunt){
 		uglify: {
 			options: {
 				mangle: false,
-				sourceMap: false        		
+				sourceMap: false
 			},
 			my_target: {
 			  files: {
@@ -62,10 +62,10 @@ module.exports = function(grunt){
 				'bundled/js/projections.js.min.js' : ['combined/js/projections/projections.js']
 			  }
 			}
-  		}		
+  		}
 	});
     // load tasks
-	grunt.loadNpmTasks('grunt-contrib-concat');	
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify-es');    
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
 };
